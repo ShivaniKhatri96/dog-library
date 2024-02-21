7a21161c30e868d1f8b4aca70c51afb8ac4847c1
Shivani Khatri <khatri.shivani96@gmail.com>
2024-02-21 21:32:35 +0100

    dog data mapped and added in a component

--- app/components/GridDog.tsx
+++ app/components/GridDog.tsx
@@ -1,8 +1,23 @@
-
-
-const GridDog = () => {
+'use client'
+import styles from '../page.module.css'
+type gridType = {
+  allDogs: any,
+  setAllDogs: any
+}
+const GridDog = ({allDogs, setAllDogs}: gridType) => {
+  if(!allDogs.length) return
   return (
-    <div>GridDog</div>
+    <div className={styles.allCards}>
+     { allDogs?.map((dog:any) => 
+     <div key={dog.id}>
+      {/* dog.url: "https://cdn2.thedogapi.com/images/S1T8Ee9Nm_1280.jpg" */}
+      <div>Name: {dog?.breeds[0]?.name}</div>
+      <div>Temperament: {dog?.breeds[0]?.temperament}</div>
+      <div>Breed group: {dog?.breeds[0]?.bred_group}</div>
+      <div>Breed for: {dog?.breeds[0]?.bred_for}</div>
+      <div>life span: {dog?.breeds[0]?.life_span}</div>
+     </div>)}
+    </div>
   )
 }
 
--- app/page.tsx
+++ app/page.tsx
@@ -7,23 +7,24 @@ import GridDog from "./components/GridDog";
 export default function Home() {
   const [allDogs, setAllDogs] = useState<any[]>([]);
   useEffect(() => {
-    const url = "https://api.thedogapi.com/v1/images/search?limit=30&api_key=live_ZpR1zhvt8RaGCvGRWKt8zf2rWK5aDTE2ns2tao5jBJ6eUVHeKOZkXoyrMsziASrZ";
+    const url = "https://api.thedogapi.com/v1/images/search?limit=40&api_key=live_ZpR1zhvt8RaGCvGRWKt8zf2rWK5aDTE2ns2tao5jBJ6eUVHeKOZkXoyrMsziASrZ";
     const fetchData = async () => {
       try {
         const response = await fetch(url);
         const data = await response.json();
-        setAllDogs(data);
+        const filteredData = data.filter((item: any) => item?.breeds && item?.breeds.length  !== 0)
+        setAllDogs(filteredData);
       } catch (error) {
         console.log("error", error);
       }
     };
     fetchData();
   }, []);
-  console.log(allDogs)
+
   return (
     <main className={styles.main}>
      <div>Dog Library</div>
-     <GridDog />
+     <GridDog allDogs={allDogs} setAllDogs={setAllDogs}/>
     </main>
   );
 }
