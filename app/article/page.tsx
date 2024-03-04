a18b3e29f190522bcc553582bfa289fc2900b433
Shivani Khatri <khatri.shivani96@gmail.com>
2024-03-04 02:32:08 +0100

    animation added to article page if the object is empty

--- app/article/page.tsx
+++ app/article/page.tsx
@@ -1,16 +1,21 @@
 //localStorage is client-side
 "use client";
+import NotFound from "@/components/NotFound";
 import styles from "./article.module.css";
 
 const Article = () => {
   const dogArticle = sessionStorage.getItem("dogArticle");
   const article = JSON.parse(dogArticle || "{}");
   console.log("article", article);
-
   return (
     <main className={styles.articleBg}>
-      <div className={styles.article}>Article</div>
-      {/* <div>Temperament: {dog?.breeds[0]?.temperament}</div> */}
+      {Object.keys(article).length === 0 ? (
+        <div className={styles.notFoundBox}>
+          <NotFound />
+        </div>
+      ) : (
+        <div className={styles.article}>Article</div>
+      )}
     </main>
   );
 };
